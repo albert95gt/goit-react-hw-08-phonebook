8017e921c08acaf174d6b10a5caffb8f8a067e86
albert95gt <84626708+albert95gt@users.noreply.github.com>
2022-05-09 18:07:55 +0000

    Deploying to gh-pages from @ albert95gt/goit-react-hw-08-phonebook@c2523d569d3226afd4a623c8d971caa911a7f223 🚀

---
+++ static/js/983.dc5e5ee2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Home Page"})}}}]);
+//# sourceMappingURL=983.dc5e5ee2.chunk.js.map
