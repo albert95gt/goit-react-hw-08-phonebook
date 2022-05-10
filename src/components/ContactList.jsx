1792dee0ddb1cbe0310752a26fc0f02e9db935d1
albert95gt <albitakacs@gmail.com>
2022-05-10 17:45:00 +0200

    fixed navigation component

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -5,21 +5,23 @@ import BounceLoader from 'react-spinners/BounceLoader';
 import { useSelector } from 'react-redux';
 import { useGetContactsQuery } from 'redux/contacts/contactApi';
 import { contactsSelectors } from 'redux/contacts';
+import { authSelectors } from 'redux/auth';
 
 const ContactList = () => {
   const filter = useSelector(contactsSelectors.getFilter);
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
 
   const { contacts, isFetching } = useGetContactsQuery(undefined, {
     selectFromResult: ({ data, isFetching }) => ({
       contacts: data ? contactsSelectors.getVisibleContacts(data, filter) : [],
       isFetching,
     }),
-    // refetchOnMountOrArgChange: true,
+    refetchOnMountOrArgChange: isLoggedIn,
   });
 
   return (
     <ul>
-      {isFetching && <BounceLoader color="orange" />}
+      {isFetching && !contacts && <BounceLoader color="orange" />}
       {contacts &&
         contacts.map(contact => {
           return <ContactItem key={contact.id} contact={contact}></ContactItem>;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,13 @@
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { authSelectors } from 'redux/auth';
+
 export default function Navigation() {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   return (
     <nav>
       <NavLink to="/">Homepage</NavLink>
-      <NavLink to="/contacts">Contacts</NavLink>
+      {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
     </nav>
   );
 }
