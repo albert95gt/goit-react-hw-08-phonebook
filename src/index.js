aad65ffcf1ae9f9ace023488de04aacbb40ef10f
albert95gt <albitakacs@gmail.com>
2022-05-15 08:45:27 +0200

    fixed base url

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
           <App />
         </BrowserRouter>
       </PersistGate>
